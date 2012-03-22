8e2a8b777fdda64e3b0ab11f033d9e65100138a6
The Hexx <greaterhexx@gmail.com>
2012-03-22 22:30:32 +0900

    一通り中身を書く

--- build.sbt
+++ build.sbt
@@ -11,7 +11,7 @@ scalaVersion := "2.9.1"
 scalacOptions += "-deprecation"
 
 libraryDependencies ++= Seq(
-  "com.github.scopt" %% "scopt" % "2.0.0",
+  "com.github.scopt" %% "scopt" % "1.1.3",
   "com.twitter" % "util-eval" % "1.12.13",
   "com.github.hexx" %% "dispatch-tumblr" % "0.0.1",
   "org.specs2" %% "specs2" % "1.8.2" % "test"
--- src/main/scala/Scalamblr.scala
+++ src/main/scala/Scalamblr.scala
@@ -1,18 +1,84 @@
 package com.github.hexx.scalamblr
 
+trait Config {
+  def defaultHostname: String
+  def markdown = false
+  def consumerKey: String
+  def consumerSecret: String
+  def username: String
+  def password: String
+}
+
 class Scalamblr extends xsbti.AppMain {
   def run(config: xsbti.AppConfiguration) = {
-    Exit(Scalamblr.run(config.arguments))
+    Scalamblr.main(config.arguments)
+    Exit(0)
   }
 }
 
 object Scalamblr {
-  def run(args: Array[String]): Int = {
-    println("Hello World: " + args.mkString(" "))
-    0
-  }
+  import com.twitter.util.Eval
+
+  val scalamblrConfigFile = scala.util.Properties.userHome + "/.scalamblr/config.scala"
+
+  val eval = new Eval
+  val config = eval[Config](new java.io.File(scalamblrConfigFile))
+
+  case class Option(var hostname: String = config.defaultHostname,
+                    var title: String = "",
+                    var tags: String = "",
+                    var body: String = "",
+                    var markdown: Boolean = false,
+                    var help: Boolean = false)
+
   def main(args: Array[String]) {
-    System.exit(run(args))
+    import scopt._
+
+    var option = Option()
+
+    val parser = new OptionParser("scalamblr") {
+      opt("hostname", "hostname", { v => option.hostname = v })
+      opt("title", "title", { v => option.title = v })
+      opt("tags", "tags", { v => option.tags = v })
+      opt("markdown", "use markdown", { option.markdown = true })
+      opt("h", "help", "help", { option.help = true })
+      opt("body", "post body", { v => option.body = v})
+    }
+    if (!parser.parse(args)) {
+      return
+    }
+    if (option.help) {
+      println(parser.usage)
+      return
+    }
+
+    def readBody: String = {
+      import scala.io.Source
+
+      if (option.body.length > 0) {
+        return Source.fromFile(option.body).mkString
+      } else {
+        return Source.stdin.mkString
+      }
+    }
+
+    def post(body: String) {
+      import dispatch._
+      import dispatch.oauth.Consumer
+      import com.github.hexx.dispatch.tumblr._
+
+      val consumer = Consumer(config.consumerKey, config.consumerSecret)
+      val http = new Http
+      val access_token = http(Auth.access_token(consumer, config.username, config.password))
+      val query = Seq("type" -> "text",
+                      "title" -> option.title,
+                      "tags" -> option.tags,
+                      "markdown" -> option.markdown.toString,
+                      "body" -> body)
+      http(Blog.post(option.hostname, consumer, access_token, query:_*))
+    }
+
+    post(readBody)
   }
 }
 
